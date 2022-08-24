fcf841fe85fc9d1d03e56b313f54bf2a6544f723
Sirii28 <111787652+Sirii28@users.noreply.github.com>
2022-08-24 19:17:59 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿{
+
+    Console.WriteLine("Enter your name:");
+    string name = Console.ReadLine() ?? "";
+
+   // if (name == null)
+     //   name = "";
+    
+    switch(name)  
+    {
+       case "Irada":
+            Console.WriteLine("Your name is Irada");
+            break;
+        default:
+            Console.WriteLine("Wrong");
+            break;
+    }
+
+    Console.WriteLine("Enter your age:");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    if (age >= 18) 
+    Console.WriteLine("Ok");
+
+    else if (age == 18)
+        Console.WriteLine("Almost Ok");
+
+    else
+        Console.WriteLine("Wrong");
+
+    Console.WriteLine("enter your weight");
+
+    double weight = Convert.ToDouble(Console.ReadLine());
+
+    if (weight > 60)
+        Console.WriteLine("you dont fit us");
+
+    else if (weight >= 60)
+        Console.WriteLine("ok");
+
+    else if (weight < 60)
+        Console.WriteLine("ok");
+
+    else
+    
+        Console.WriteLine("you dont fit us");
+        
+    Console.WriteLine("if you have more than 2 correct answers, you are accepted");
+    int answer = Convert.ToInt32(Console.ReadLine());
+
+    if (answer >= 2)
+        Console.WriteLine("you are accepted");
+    else if (answer <= 3)
+        Console.WriteLine("You are not accepted");
+
+    Console.WriteLine("Adress is: Mederova 123");
+
+
+
+    
+       
+
+
+
+
+
+    
+
+    
+
+
+
+        
+
+
+
+}
